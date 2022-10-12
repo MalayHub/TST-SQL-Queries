310bb284fda5c789056f3abd3018b31c2a3c7d40
shreya thale <shreyathale22@gmail.com>
2022-10-12 15:15:32 +0530

    Query files added for roles & rights

---
+++ MST_Roles_CreateQuery.sql
@@ -0,0 +1,26 @@
+USE [TST]
+GO
+
+/****** Object:  Table [dbo].[MST_Roles]    Script Date: 12-10-2022 14:49:06 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[MST_Roles](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Name] [nvarchar](max) NOT NULL,
+	[CreatedBy] [uniqueidentifier] NOT NULL,
+	[CreatedDate] [datetime2](7) NOT NULL,
+	[ModifiedBy] [uniqueidentifier] NULL,
+	[ModifiedDate] [datetime2](7) NULL,
+	[IsDeleted] [bit] NOT NULL,
+	[DeletedDate] [datetime2](7) NULL,
+	[DeletedBy] [uniqueidentifier] NULL,
+ CONSTRAINT [PK_MST_Roles] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
