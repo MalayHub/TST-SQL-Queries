071080845cf30b53f1ce557ed7d92cb1c4ec871e
jagrutgala <jagrutgala@gmail.com>
2022-10-12 15:57:19 +0530

    Updated table names as per convention
    current table names are
    - MST_Rights
    - MST_Roles
    - MST_Users

--- MST_Rights_CreateQuery.sql
+++ MST_Rights_CreateQuery.sql
@@ -1,14 +1,14 @@
 USE [TST]
 GO
 
-/***** Object:  Table [dbo].[MST_Right]    Script Date: 10/12/2022 2:49:31 PM *****/
+/***** Object:  Table [dbo].[MST_Rights]    Script Date: 10/12/2022 2:49:31 PM *****/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[MST_Right](
+CREATE TABLE [dbo].[MST_Rights](
 	[Id] [uniqueidentifier] NOT NULL,
 	[Name] [nvarchar](max) NOT NULL,
 	[CreatedBy] [uniqueidentifier] NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE [dbo].[MST_Right](
 	[IsDeleted] [bit] NOT NULL,
 	[DeletedDate] [datetime2](7) NULL,
 	[DeletedBy] [uniqueidentifier] NULL,
- CONSTRAINT [PK_MST_Right] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_MST_Rights] PRIMARY KEY CLUSTERED
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
